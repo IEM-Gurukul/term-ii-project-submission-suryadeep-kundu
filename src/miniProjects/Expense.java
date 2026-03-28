8f7d0487d2441ea2ca227dfc2743ccb81d0a51fe
sunny <kundusuryadeep@gmail.com>
2026-03-28 19:38:29 +0530

    Add Expense.java: Serializable class for expense data

---
+++ src/miniProjects/Expense.java
@@ -0,0 +1,21 @@
+package miniProjects;
+
+import java.io.Serializable;
+
+public class Expense implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private double amount;
+    private String description;
+
+    public Expense(double amount, String description) {
+        this.amount = amount;
+        this.description = description;
+    }
+
+    public double getAmount() { return amount; }
+    public String getDescription() { return description; }
+
+    public void setAmount(double amount) { this.amount = amount; }
+    public void setDescription(String description) { this.description = description; }
+}
